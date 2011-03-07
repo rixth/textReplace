49170dc2e573b03c671ec48c57c832db90da8735
Tom Rix <tom@rixth.org>
2011-03-07 08:27:39 -0800

    fixing tests

--- test/textReplace.test.js
+++ test/textReplace.test.js
@@ -14,13 +14,8 @@ test("regex search", function () {
 
 test("callback replace", function () {
   expect(1);
-  // $('#qunit-fixture p.repeat').textReplace(/He[l]{1,2}o/g, function (match) {
-  //   return match.toUpperCase();
-  // });  
-  // equals($('#qunit-fixture p.repeat').html(), "HELLO, world. HELLO, world.");
-  $('#qunit-fixture p.repeat').html('Hello my_snakecase_method world.')
-  $('#qunit-fixture p.repeat').textReplace(/_[a-z]/g, function (match) {
-    return match.substr(1).toUpperCase();
+  $('#qunit-fixture p.repeat').textReplace(/He[l]{1,2}o/g, function (match) {
+    return match.toUpperCase();
   });  
   equals($('#qunit-fixture p.repeat').html(), "HELLO, world. HELLO, world.");
-});
+y});
